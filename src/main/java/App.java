796e232027fc926226f67738cd098d8c61de626e
Albertino274 <albertino274@mail.ru>
2025-02-13 17:26:12 +0300

    2.1.2 Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanSecond = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+
+        Cat catSecond = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Are helloWorld's beans equals? - " + (bean == beanSecond));
+
+        System.out.println("Are cat's beans equals? - " + (cat == catSecond));
     }
 }
